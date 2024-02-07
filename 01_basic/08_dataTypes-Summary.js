57a8eabd3bd7919056d293d08c8e8eb3081130bc
Chhavi9700225780 <c9081741@gmail.com>
2024-02-07 11:02:31 +0530

    Basics of Javasript

---
+++ 01_basic/08_dataTypes-Summary.js
@@ -0,0 +1,25 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
