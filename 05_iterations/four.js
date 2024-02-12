b332150b4b1ff5e6e7fdd8005d41cddc51be457d
Chhavi9700225780 <c9081741@gmail.com>
2024-02-12 21:17:32 +0530

    forIn Loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);// ans. values js , rb , py , java, cpp
+     // console.log(key)// ans. 0 1 2 3 4
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// } // not display on screen because it is not itratable so for in loop on map is not applicabe
