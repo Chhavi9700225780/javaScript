1810d7fe66f9631b7976ec0cc9209f39e7773823
Chhavi9700225780 <c9081741@gmail.com>
2024-02-15 11:43:11 +0530

    Digital Clock

--- 07_projects.js/project-2/pro.js
+++ 07_projects.js/project-2/pro.js
@@ -17,5 +17,7 @@ form.addEventListener('submit', function (e) {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
     results.innerHTML = `<span>${bmi}</span>`;
+    
   }
+
 });
