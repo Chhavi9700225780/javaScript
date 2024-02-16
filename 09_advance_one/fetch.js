89cf8518740953f27c0baba4cd349d22aa2aefab
Chhavi9700225780 <c9081741@gmail.com>
2024-02-16 12:08:01 +0530

    Fetch

---
+++ 09_advance_one/fetch.js
@@ -0,0 +1,16 @@
+//fetch is a library
+//syntax
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
+//here data comes first before all the code //Why??
+//Questions when we send request to browser then a error comes in response is it come in response or in error
+//ans response
