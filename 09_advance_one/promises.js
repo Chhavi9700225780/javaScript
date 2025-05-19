e72257d678601286b2aabfcfad48fd7045573d18
Chhavi9700225780 <156224346+Chhavi9700225780@users.noreply.github.com>
2025-05-19 15:40:34 +0530

    Update promises.js

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -90,6 +90,7 @@ async function consumePromiseFive(){
 consumePromiseFive()
 
 
+
  async function getAllUsers(){
      try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
